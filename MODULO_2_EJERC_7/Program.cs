7105829248e055cc94a01e1ec4d61d9eb4acd84c
cami63830 <cami63830@gmail.com>
2018-09-29 21:28:03 -0300

    OK

--- MODULO_2_EJERC_7/Program.cs
+++ MODULO_2_EJERC_7/Program.cs
@@ -10,6 +10,15 @@ namespace MODULO_2_EJERC_7
     {
         static void Main(string[] args)
         {
+
+            //Tarea 7: Crea un programa que calcule la suma de 200 y 1111, usando variables.
+            int K1, G1, P1;
+            K1 = 200;
+            G1 = 1111;
+            P1 = K1 + G1;
+            Console.WriteLine("Ejercicio 7: el producto con variables de 200*1111={0}", P1);
+            Console.ReadKey();
+
         }
     }
 }
